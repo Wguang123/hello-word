e7ab066cb8552e837a2037cd94d2ea35b0f0024e
weiguang <weiguang.wg@alibaba-inc.com>
2019-01-16 22:27:21 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import Home from './components/Home'
 
 class App extends Component {
-  render() {
-    return (
-        <div className="container">
-          <div className="raw">
+    render() {
 
-            <div className="col-xs-1 clo-xs-offset-11">
-              <h1>Hello!</h1>
+        const user={
+            name:"weiguang",
+            age:12,
+            hobbies:["sports","reading"]
+        };
+        return (
+            <div className="container">
+                <div className="raw">
+                    <div className="col-xs-1 clo-xs-offset-11">
+                        <Header> Header</Header>
+                    </div>
+                </div>
+
+                <div className="raw">
+                    <div className="col-xs-1 clo-xs-offset-11">
+                    </div>
+                </div>
+
+                <div className="raw">
+                    <div className="col-xs-1 clo-xs-offset-11">
+                        <Home initAge={28} cm  user={user}>
+                            <p> I am child </p>
+                        </Home>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 
